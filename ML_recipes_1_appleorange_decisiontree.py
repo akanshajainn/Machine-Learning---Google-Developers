6558444782e0cc7b58e1b8cf0707d91aea62748d
Akansha Jain <akanshajain@Apples-MacBook-Air.local>
2017-09-29 18:10:41 +0530

    first

---
+++ ML_recipes_1_appleorange_decisiontree.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import sklearn
+from sklearn import tree
+#features=[[140,'smooth'],[130,'smooth'],[150,'bumpy'],[170,'bumpy']]
+#labels=['apple','apple','orange','orange']
+features=[[140,1],[130,1],[150,0],[170,0]]
+labels=[0,0,1,1]
+clf=tree.DecisionTreeClassifier()
+clf=clf.fit(features,labels)
+print(clf.predict([[130,1]]))
---
+++ ML_recipes_2_iris.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 29 16:39:36 2017
+
+@author: akanshajain
+"""
+from sklearn.datasets import load_iris
+import numpy as np
+from sklearn import tree
+#Know Dataset IRIS
+iris=load_iris()
+print(iris.feature_names)
+print(iris.target_names)
+print(iris.data[0])
+print(iris.target[0])
+for i in range(len(iris.target)):
+    print('Example %d: label %s, features %s'%(i,iris.target[i],iris.data[i])) 
+
+#Splitting in Train Test
+test_idx=[0,50,100]
+#training data
+train_target=np.delete(iris.target,test_idx)
+train_data=np.delete(iris.data,test_idx,axis=0)
+#testing data
+test_target=iris.target[test_idx]
+test_data=iris.data[test_idx]
+#Training
+clf=tree.DecisionTreeClassifier()
+clf.fit(train_data,train_target)
+#Predicting
+print(test_target)
+print(clf.predict(test_data))
+#Visualisation
+from sklearn.externals.six import StringIO
+import pydotplus
+dot_data=StringIO()
+tree.export_graphviz(clf,
+                         out_file=dot_data,
+                         feature_names=iris.feature_names,
+                         class_names=iris.target_names,
+                         filled=True, rounded=True,
+                         impurity=False)
+graph=pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_pdf("iris.pdf")
